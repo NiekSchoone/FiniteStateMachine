3070516afe3cd243d26eabf602625b291078bd18
Niek Schoone <niek.schoone@portico.nl>
2020-03-24 21:05:55 +0100

    better forests

--- Assets/Scripts/World/Flora/FloraManager.cs
+++ Assets/Scripts/World/Flora/FloraManager.cs
@@ -5,32 +5,47 @@ using UnityEngine;
 public class FloraManager : MonoBehaviour
 {
   [SerializeField]
-  private GameObject surface;
-
-  [SerializeField]
   private float radius;
 
   [SerializeField]
   private float minDistanceBetweenFlora;
 
   [SerializeField]
-  private float spreadRate;
+  private float timeBetweenGrowths;
 
   private float timeToNext;
 
   [SerializeField]
   private GameObject floraToGrow;
 
+  [SerializeField]
+  private int maxToGrow;
+
   private List<GameObject> grownFlora;
 
   void Start() {
     grownFlora = new List<GameObject>();
-
-    GameObject firstToGrow = GameObject.Instantiate(floraToGrow, transform.position, Quaternion.identity);
+    timeToNext = 0;
   }
 
   void Update() {
-
+    timeToNext -= Time.deltaTime;
+    if (grownFlora.Count < maxToGrow && timeToNext <= 0) {
+      Grow();
+    }
   }
 
+  void Grow() {
+    timeToNext = timeBetweenGrowths;
+    GameObject newGrowth = GameObject.Instantiate(floraToGrow, RandomPointInBox(this.transform.position, new Vector3(radius, 0, radius)), Quaternion.identity);
+    grownFlora.Add(newGrowth);
+  }
+
+  Vector3 RandomPointInBox(Vector3 center, Vector3 size) {
+    return center + new Vector3(
+       (Random.value - 0.5f) * size.x,
+       (Random.value - 0.5f) * size.y,
+       (Random.value - 0.5f) * size.z
+    );
+  }
 }
--- Assets/Scripts/World/Flora/Tree.cs
+++ Assets/Scripts/World/Flora/Tree.cs
@@ -4,5 +4,26 @@ using UnityEngine;
 
 public class Tree : MonoBehaviour
 {
-    
+  [SerializeField]
+  private float toppleAngle;
+
+  [SerializeField]
+  private bool chopped;
+
+  private float fall;
+
+  void Start() {
+    this.transform.eulerAngles = new Vector3(
+      this.transform.eulerAngles.x,
+      this.transform.eulerAngles.y + Random.Range(0, 360),
+      this.transform.eulerAngles.z
+    );
+  }
+
+  private void Update() {
+    if(this.chopped && fall < 1) {
+      fall += Time.deltaTime/2;
+      this.transform.eulerAngles = new Vector3(this.transform.eulerAngles.x, this.transform.eulerAngles.y, Mathf.SmoothStep(0, toppleAngle, fall));
+    }
+  }
 }
